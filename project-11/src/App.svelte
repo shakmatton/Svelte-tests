4212508e71cd227c73200be2c4e84a7e766a7f86
shakmatton <ezio.auditore0@protonmail.com>
2023-03-13 00:57:46 -0300

    Commit net_ninja_project_11

---
+++ project-11/src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Modal from './Modal.svelte';
+
+	let people = [
+    { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+    { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+    { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+  ];
+
+  const handleClick = (e, id) => {
+    people = people.filter(person => person.id != id);
+    console.log(e);
+  };
+</script>
+
+<Modal message='Hello, Ninjas!' isPromo={true} />     <!-- Those "arguments" are called "Props" -->
+<main>                                       <!-- The advantage of using Props is to reuse them anywhere -->
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      {#if person.beltColour === 'black'}
+        <p><strong>MASTER NINJA</strong></p>
+      {/if}
+      <p>{person.age} years old, {person.beltColour} belt.</p>
+      <button on:click={(e) => handleClick(e, person.id)}>delete</button>
+    </div>
+  {:else}
+    <p>There are no people to show...</p>
+  {/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
